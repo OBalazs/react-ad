8b7a7d8e6f09b2cba3c7641dda25cff856a345e2
OBalazs <ocsibalazs@gmail.com>
2026-01-07 21:16:48 +0100

    azure config

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 export const config = {
   appId: '6c46978b-a779-42ca-b8d4-3357bcf2e7c1',
-  redirectUri: 'http://localhost:3000',
+  redirectUri: 'https://lively-bush-0378ddb03.4.azurestaticapps.net/',
   scopes: [
     'user.read',
   ],
